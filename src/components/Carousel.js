7ef0a659b2e0c6a0875a7b30aca480c8f4c4ba72
AMillsy <00adammills00@gmail.com>
2023-09-04 18:29:12 +0100

    adding buttons to the carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,15 +1,44 @@
 import img1 from "../images/img1.jpeg";
 import img2 from "../images/img2.png";
+import React from "react";
 import "../styles/Carousel.css";
 const Carousel = () => {
+  let isDragStart = false,
+    prevPageX,
+    prevScrollLeft;
+
+  const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX;
+    prevScrollLeft = carouselRef.current.scrollLeft;
+  };
+  const dragStop = () => {
+    isDragStart = false;
+  };
+  function dragging(e) {
+    if (!isDragStart) return;
+    e.preventDefault();
+    let positionDiff = e.pageX - prevPageX;
+    carouselRef.current.scrollLeft = prevScrollLeft - positionDiff;
+  }
+  const carouselRef = React.createRef();
   return (
-    <div>
+    <div className="carousel">
       <h3 className="carouselTitle">Title</h3>
-      <div className="carousel">
-        <div className="carouselImages">
-          <img src={img1}></img>
-        </div>
-        <img className="carouselImages" src={img2} />
+
+      <div
+        className="container"
+        onMouseMove={(e) => dragging(e)}
+        onMouseDown={(e) => dragStart(e)}
+        onMouseUp={() => dragStop()}
+        ref={carouselRef}
+      >
+        <button className="carouselButtons left">&lt;</button>
+        <img className="carouselImages" src={img1} alt="" />
+        <img className="carouselImages" src={img2} alt="" />
+        <img className="carouselImages" src={img2} alt="" />
+        <img className="carouselImages" src={img2} alt="" />
+        <button className="carouselButtons right">&gt;</button>
       </div>
     </div>
   );
