2897e8834d3bd6687a67a901a8fb0db32b646539
AMillsy <00adammills00@gmail.com>
2023-09-05 12:36:58 +0100

    making carousel smooth scroll with buttons

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,19 +1,30 @@
 import img1 from "../images/img1.jpeg";
 import img2 from "../images/img2.png";
-import React from "react";
+import { createRef, useRef, useState, useLayoutEffect } from "react";
 import "../styles/Carousel.css";
 const Carousel = () => {
   let isDragStart = false,
     prevPageX,
     prevScrollLeft;
 
+  const carouselRef = createRef(null);
+  let firstImageRef = useRef(null);
+
+  const [width, setWidth] = useState(0);
+
+  useLayoutEffect(() => {
+    setWidth(firstImageRef.offsetWidth);
+  });
+
   const dragStart = (e) => {
+    carouselRef.current.style.scrollBehavior = "auto";
     isDragStart = true;
     prevPageX = e.pageX;
     prevScrollLeft = carouselRef.current.scrollLeft;
   };
   const dragStop = () => {
     isDragStart = false;
+    carouselRef.current.style.scrollBehavior = "smooth";
   };
   function dragging(e) {
     if (!isDragStart) return;
@@ -21,10 +32,21 @@ const Carousel = () => {
     let positionDiff = e.pageX - prevPageX;
     carouselRef.current.scrollLeft = prevScrollLeft - positionDiff;
   }
-  const carouselRef = React.createRef();
+
+  const moveCarousel = (e) => {
+    const isLeft = e.target.classList.contains("left");
+
+    if (isLeft) {
+      carouselRef.current.scrollLeft -= width;
+      return console.log("This is left");
+    }
+    carouselRef.current.scrollLeft += width;
+    return console.log("This is right");
+  };
+
   return (
     <div className="carousel">
-      <h3 className="carouselTitle">Title</h3>
+      <h3 className="carouselTitle">Models</h3>
 
       <div
         className="container"
@@ -33,12 +55,31 @@ const Carousel = () => {
         onMouseUp={() => dragStop()}
         ref={carouselRef}
       >
-        <button className="carouselButtons left">&lt;</button>
-        <img className="carouselImages" src={img1} alt="" />
+        <button
+          className="carouselButtons left"
+          onClick={(e) => moveCarousel(e)}
+        >
+          &lt;
+        </button>
+        <img
+          className="carouselImages"
+          src={img1}
+          alt=""
+          ref={(e) => (firstImageRef = e)}
+        />
+        <img ref={firstImageRef} className="carouselImages" src={img2} alt="" />
         <img className="carouselImages" src={img2} alt="" />
         <img className="carouselImages" src={img2} alt="" />
         <img className="carouselImages" src={img2} alt="" />
-        <button className="carouselButtons right">&gt;</button>
+        <img className="carouselImages" src={img2} alt="" />
+        <img className="carouselImages" src={img2} alt="" />
+        <img className="carouselImages" src={img2} alt="" />
+        <button
+          className="carouselButtons right"
+          onClick={(e) => moveCarousel(e)}
+        >
+          &gt;
+        </button>
       </div>
     </div>
   );
